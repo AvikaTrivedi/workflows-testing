86433263110ba18fb4b3c07911d22fbc7ece2328
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2022-02-14 18:13:54 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,13 +1,14 @@
 apt-get update -y && apt-get upgrade -y
 apt-get install ffmpeg -y
-apt-get install avconv -y
 apt install neofetch
 git clone -b dev https://github.com/TeamUltroid/Ultroid 
 chmod 777 Ultroid
 cp SedLoif/.env Ultroid/.env
-cd Ultroid 
+chmod 777 Ultroid
+cd Ultroid
+chmod +x startup
 pip3 install --no-cache-dir -r requirements.txt 
 pip3 install --no-cache-dir -r res*/sta*/opt*.txt
 pip3 install yt_dlp -U
 pip3 install av --no-binary av
-python3 -m pyUltroid
+bash startup
