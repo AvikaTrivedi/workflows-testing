0649f19a1b9bf575d2f2335fa675f90f6fdc125a
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2022-02-16 07:32:09 +0530

    Huh

--- action.sh
+++ action.sh
@@ -1,6 +1,11 @@
-apt-get update -y && apt-get upgrade -y
-apt-get install ffmpeg -y
-apt install neofetch
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install pyenv
+pyenv install 3.9.2
+brew install git
+brew install ffmpeg
+brew install neofetch
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
 git clone -b dev https://github.com/TeamUltroid/Ultroid 
 chmod 777 Ultroid
 cp SedLoif/.env Ultroid/.env
@@ -11,4 +16,4 @@ pip3 install --no-cache-dir -r requirements.txt
 pip3 install --no-cache-dir -r res*/sta*/opt*.txt
 pip3 install yt_dlp -U
 pip3 install av --no-binary av
-bash startup
+sh startup
