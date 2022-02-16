7f65d5d6bd6cbf61cc7fa5cfd03c3c0c18ab2764
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2022-02-16 13:36:24 +0530

    Huh

--- action.sh
+++ action.sh
@@ -1,19 +1,8 @@
-
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-sudo chown $USER /usr/local/bin/brew
-brew update && brew upgrade
-brew install neofetch
-brew install ffmpeg
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
+apt-get update -y && apt-get upgrade -y && apt-get install ffmpeg -y && apthget install neofetch -y && apt-get install mediainfo
 git clone -b dev https://github.com/TeamUltroid/Ultroid 
 cp SedLoif/.env Ultroid/.env
 chmod 777 Ultroid
 cd Ultroid
 chmod +x startup
-pip3 install -r requirements.txt 
-pip3 install -r resources/startup/optional-requirements.txt
-pip3 install aiohttp 
-pip3 install --no-cache-dir --ignore-installed yt_dlp 
-pip3 install av --no-binary av
-sh startup
+pip3 install --no-cache-dir --ignore-installed -r requirements.txt && pip3 install --no-cache-dir --ignore-installed -r resources/startup/optional-requirements.txt && pip3 install --ignore-installed aiohttp && pip3 install --no-cache-dir --ignore-installed yt_dlp && pip3 install av --no-binary av
+bash startup
