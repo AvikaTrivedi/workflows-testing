2659993cc7807853630efd248ae7c7471e5f04d5
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2022-02-16 11:34:26 +0530

    Huh

--- action.sh
+++ action.sh
@@ -1,18 +1,24 @@
+curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.7.1.tar.bz2
+tar xf MacPorts-2.7.1.tar.bz2
+cd MacPorts-2.7.1/
+./configure
+make
+sudo make install
+cd ..
+sudo port install ffmpeg
+sudo port install neofetch
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew update && sudo brew upgrade
-brew install git
-brew install ffmpeg
-brew install neofetch
+brew update && brew upgrade
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
-git clone -b dev https://github.com/TeamUltroid/Ultroid 
-chmod 777 Ultroid
-cp SedLoif/.env Ultroid/.env
-chmod 777 Ultroid
-cd Ultroid
+git clone -b dev https://github.com/TeamUltroid/Ultroid /root/Ultroid
+cp SedLoif/.env /root/Ultroid/.env
+chmod 777 /root/Ultroid
+cd /root/Ultroid
 chmod +x startup
 pip3 install -r requirements.txt 
 pip3 install -r resources/startup/optional-requirements.txt
-pip3 install aiohttp -U
+pip3 install aiohttp 
+pip3 install --no-cache-dir --ignore-installed yt_dlp 
 pip3 install av --no-binary av
 sh startup
