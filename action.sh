14c0e79fcb2c682dfd5adf23718d8d23c23e9635
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2022-02-16 09:20:46 +0530

    Hurrr

--- action.sh
+++ action.sh
@@ -1,12 +1,8 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-sudo chown -R `whoami` /usr/local/Homebrew/
-sudo chown -R $(whoami) $(brew --prefix)/*
-sudo mkdir /usr/local/Frameworks
-sudo chown -R `whoami` /usr/local/Frameworks/
-brew update && brew upgrade
-brew install git
-brew install ffmpeg
-brew install neofetch
+sudo brew update && sudo brew upgrade
+sudo brew install git
+sudo brew install ffmpeg
+sudo brew install neofetch
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 git clone -b dev https://github.com/TeamUltroid/Ultroid 
@@ -17,7 +13,6 @@ cd Ultroid
 chmod +x startup
 pip3 install -r requirements.txt 
 pip3 install -r resources/startup/optional-requirements.txt
-pip3 install yt-dlp -U
 pip3 install aiohttp -U
 pip3 install av --no-binary av
 sh startup
