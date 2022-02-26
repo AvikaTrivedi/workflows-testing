4ad94b8eb713c56db774197210dc634ffa2e283c
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2022-02-26 09:19:03 +0530

    Huhhhhshshs

--- action.sh
+++ action.sh
@@ -1,8 +1,12 @@
-apt-get update -y && apt-get upgrade -y
+apt-get update -y && apt-get upgrade -y 
+apt-get install neofetch -y 
+apt-get install mediainfo -y
+apt-get install ffmpeg -y
 clear
 git clone -b dev https://github.com/TeamUltroid/Ultroid 
 cp SedLoif/.env Ultroid/.env
 chmod 777 Ultroid
 cd Ultroid
 chmod +x startup
-apt-get update -y && apt install ffmpeg mediainfo neofetch -y && python3 -m pip install --upgrade pip setuptools wheel && python3 -m pip install --no-cache-dir --ignore-installed --force-reinstall --no-warn-conflicts -U -r req* && python3 -m pip install --no-cache-dir --ignore-installed --force-reinstall --no-warn-conflicts -U -r res*/sta*/opt*.txt && clear && bash startup
+python3 -m pip install --upgrade pip setuptools wheel && python3 -m pip install --no-cache-dir --ignore-installed --force-reinstall --no-warn-conflicts -U -r req* && python3 -m pip install --no-cache-dir --ignore-installed --force-reinstall --no-warn-conflicts -U -r res*/sta*/opt*.txt && clear 
+bash startup
