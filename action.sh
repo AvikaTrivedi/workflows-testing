078fe00e3e8dee2a635819479b1c2f6b820edd08
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2022-02-26 09:07:54 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -5,4 +5,4 @@ cp SedLoif/.env Ultroid/.env
 chmod 777 Ultroid
 cd Ultroid
 chmod +x startup
-apt-get update -y && apt install ffmpeg mediainfo neofetch wkhtmltopdf libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libswresample-dev libswscale-dev -y && python3 -m pip install --upgrade pip setuptools wheel && python3 -m pip install --no-cache-dir --ignore-installed --force-reinstall --no-warn-conflicts -U -r req* && python3 -m pip install --no-cache-dir --ignore-installed --force-reinstall --no-warn-conflicts -U -r res*/sta*/opt*.txt && clear && bash startup
+apt-get update -y && apt install ffmpeg mediainfo neofetch -y && python3 -m pip install --upgrade pip setuptools wheel && python3 -m pip install --no-cache-dir --ignore-installed --force-reinstall --no-warn-conflicts -U -r req* && python3 -m pip install --no-cache-dir --ignore-installed --force-reinstall --no-warn-conflicts -U -r res*/sta*/opt*.txt && clear && bash startup
